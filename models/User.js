8764a039270f1f12a39d77e6dc341d76a99764cb
badrelkhatib <84342264+badrelkhatib@users.noreply.github.com>
2021-05-19 16:33:03 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose"),
+    { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    admin: Boolean
+});
+
+module.exports = mongoose.model("User", userSchema);
